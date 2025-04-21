184de4fa4f8848d038239f0fcc7ff05f646c9dff
Masterx43 <bastiancito.26@gmail.com>
2025-04-21 15:58:11 -0400

    Update cliente.java

--- FitLife/src/main/java/com/example/FitLife/model/cliente.java
+++ FitLife/src/main/java/com/example/FitLife/model/cliente.java
@@ -1,5 +1,9 @@
 package com.example.FitLife.model;
 
-public class cliente {
-
+public class cliente extends Usuario{
+    private Long id;
+    private ArrayList<Reserva> reservas;
+    private ArrayList<Actividad> actividades;
+    private ArrayList<Pago> pagos;
+    private ArrayList<Reseña> reseñas;
 }
