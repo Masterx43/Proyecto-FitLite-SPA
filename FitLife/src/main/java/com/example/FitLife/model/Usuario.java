8aaf24230ad0b4a861acbebae9106f592273e374
Benjamin Moretti <be.moretti@duocuc.cl>
2025-04-22 16:48:12 -0400

    Nuevo comienzo paquete model
    Reinicio del proyecto, creado el paquete model con la clase Usuario.java

---
+++ FitLife/src/main/java/com/example/FitLife/model/Usuario.java
@@ -0,0 +1,17 @@
+package com.example.FitLife.model;
+
+import java.util.Date;
+
+public class Usuario {
+    private int usuarioId;
+    private String pNombre;
+    private String sNombre;
+    private String apPaterno;
+    private String apMaterno;
+    private String email;
+    private String password;
+    private String telefono;
+    private String direccion;
+    private Date fechaRegistro;
+
+}
