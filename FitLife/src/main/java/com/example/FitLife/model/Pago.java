caa74822d2ca102d2835f70104fa7fcbdd9b3801
LUKAS <ldonoso472@gmail.com>
2025-04-22 16:40:20 -0400

    delited clases
    el benja me obligo profe

--- FitLife/src/main/java/com/example/FitLife/model/Pago.java
+++
@@ -1,5 +0,0 @@
-package com.example.FitLife.model;
-
-public class Pago {
-
-}
--- FitLife/src/main/java/com/example/FitLife/model/cliente.java
+++
@@ -1,9 +0,0 @@
-package com.example.FitLife.model;
-
-public class cliente extends Usuario{
-    private Long id;
-    private ArrayList<Reserva> reservas;
-    private ArrayList<Actividad> actividades;
-    private ArrayList<Pago> pagos;
-    private ArrayList<Reseña> reseñas;
-}
