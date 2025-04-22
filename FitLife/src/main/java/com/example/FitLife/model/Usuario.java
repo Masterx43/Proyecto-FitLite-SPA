7e6820211ed588f5267ecf63a1c94832eddb27ca
Masterx43 <bastiancito.26@gmail.com>
2025-04-22 17:02:55 -0400

    Update Usuario.java
    Ponganle las anotaciones de Lombok

--- FitLife/src/main/java/com/example/FitLife/model/Usuario.java
+++ FitLife/src/main/java/com/example/FitLife/model/Usuario.java
@@ -2,6 +2,12 @@ package com.example.FitLife.model;
 
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
     private int usuarioId;
     private String pNombre;
